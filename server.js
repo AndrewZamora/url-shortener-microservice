2c4fac06a3c6bba8b938ae05e800c9690de5d7ca
Andrew Zamora <andrewmichaelzamora@gmail.com>
2020-03-24 23:31:20 +0900

    Got post to check for valid address and write to db

--- server.js
+++ server.js
@@ -1,50 +1,60 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bodyParser = require("body-parser");
-
-var cors = require('cors');
-
-var app = express();
+const dns = require('dns');
+const cors = require('cors');
+const app = express();
+const checkUrl = url => {
+  return new Promise((resolve, reject) => {
+    dns.lookup(url, (err, address, family) => {
+      resolve({ err, address, family });
+    })
+  });
+}
 
 // Basic Configuration 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-const urlSchema = new Schema ({
+const urlSchema = new Schema({
   url: String,
   shortUrl: String
 })
 const Url = mongoose.model("Url", urlSchema);
 
-const test = new Url({url:"google",shortUrl:"short"});
-// test.save();
-
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
+app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
+
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-app.post("api/shorturl/new", function(req,res){
-  console.log("yo")
+app.post("/api/shorturl/new", async function (req, res) {
+  const url = req.body.url.replace(/^(http|https):\/\//, "");
+  const check = await checkUrl(url);
+  if (check.address) {
+    const newUrl = new Url({ url, shortUrl: "2" })
+    newUrl.save();
+  } else {
+    res.json({ "error": "invalid URL" });
+  }
   res.end()
 });
 
